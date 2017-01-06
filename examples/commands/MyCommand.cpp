06795d7f4a32d5e9699143dd6443c218041250d6
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2017-01-06 15:55:38 +0100

    Add type related helper functions to Argument
        int IntValue();
        long LongValue();
        bool BoolValue();
        std::list<std::string> ListValue(const char delimiter);
        std::list<int> IntListValue(const char delimiter);
        std::list<long> LongListValue(const char delimiter);

--- examples/commands/MyCommand.cpp
+++ examples/commands/MyCommand.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <list>
 #include "MyCommand.hpp"
 #include "../../src/ArgumentRegistry.hpp"
 #include "../../src/CommandDescriptor.hpp"
@@ -11,6 +12,14 @@ namespace Commands {
 
     if (args->FindByKey("verbose") != nullptr) {
       std::cout << "Verbose mode is active..." << std::endl;
+    }
+
+    if (args->FindByKey("ids") != nullptr) {
+      std::list<int> ids = args->FindByKey("ids")->IntListValue(',');
+      std::cout << "Provided ID list:" << std::endl;
+      for (int id : ids) {
+        std::cout << " - " << id << std::endl;
+      }
     }
   }
 
--- src/Argument.cpp
+++ src/Argument.cpp
@@ -1,3 +1,24 @@
+/*
+ * Original code by Balazs Nadasdi (http://github.com/Yitsushi/)
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any
+ * damages arising from the use of this software.
+ * Permission is granted to anyone to use this software for any
+ * purpose, including commercial applications, and to alter it and
+ * redistribute it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must
+ * not claim that you wrote the original software. If you use this
+ * software in a product, an acknowledgment in the product documentation
+ * would be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and
+ * must not be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ * distribution.
+ */
+
 #include "Argument.hpp"
 #include <string>
 #include <iostream>
@@ -5,11 +26,6 @@
 namespace Commander {
   Argument::Argument(const std::string key, const std::string value) : m_key(key), m_value(value)
   {}
-
-  Argument::~Argument()
-  {
-    std::cout << "freeee meeee...." << std::endl;
-  }
 
   bool Argument::OnlyValue()
   {
@@ -29,6 +45,65 @@ namespace Commander {
   std::string Argument::Value()
   {
     return m_value;
+  }
+
+  int Argument::IntValue()
+  {
+    return std::atoi(m_value.c_str());
+  }
+
+  long Argument::LongValue()
+  {
+    return std::atol(m_value.c_str());
+  }
+
+  bool Argument::BoolValue()
+  {
+    std::string v = m_value;
+    std::transform(v.begin(), v.end(), v.begin(), ::tolower);
+    if (v == "true" || v == "yes" || v == "y") return true;
+
+    if (std::atoi(v.c_str()) > 0) return true;
+
+    return false;
+  }
+
+  std::list<std::string> Argument::ListValue(const char delimiter)
+  {
+    size_t last_pos = 0, pos = 0;
+    std::list<std::string> value_list;
+
+    while ((pos = m_value.find(delimiter, last_pos)) != std::string::npos) {
+      value_list.push_back(m_value.substr(last_pos, pos-last_pos));
+      last_pos = pos + 1;
+    }
+    value_list.push_back(m_value.substr(last_pos));
+
+    return value_list;
+  }
+
+  std::list<int> Argument::IntListValue(const char delimiter)
+  {
+    std::list<int> value_list;
+    std::list<std::string> svalue_list = ListValue(delimiter);
+
+    for (std::string s : svalue_list) {
+      value_list.push_back(std::atoi(s.c_str()));
+    }
+
+    return value_list;
+  }
+
+  std::list<long> Argument::LongListValue(const char delimiter)
+  {
+    std::list<long> value_list;
+    std::list<std::string> svalue_list = ListValue(delimiter);
+
+    for (std::string s : svalue_list) {
+      value_list.push_back(std::atol(s.c_str()));
+    }
+
+    return value_list;
   }
 
   void Argument::Print()
