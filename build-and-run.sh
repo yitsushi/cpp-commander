9d6c6b7451ad96a1e43c7694892ffe3efe608aab
Balazs Nadasdi <yitsushi@gmail.com>
2017-01-16 22:36:34 +0100

    Make docker image smaller and quicker (#4)

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,11 +2,11 @@
 
 set -e
 
+find . \( -name CMakeFiles -o -name CMakeCache.txt \) -print | xargs rm -rf
+
 cmake -DCMAKE_BUILD_TYPE=Debug -DWITHCOVERAGE=1 .
 
 cmake --build .
 ctest
 
 make coverage
-
-coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info.cleaned
